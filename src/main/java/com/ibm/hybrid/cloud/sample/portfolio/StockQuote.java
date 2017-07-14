26580d1dbdbe37c48325478355fc56d4ccdc7aa0
John W. Alcorn <jalcorn@us.ibm.com>
2017-07-14 17:07:19 -0400

    Updated to use a Kube secret

--- src/main/java/com/ibm/hybrid/cloud/sample/portfolio/StockQuote.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/portfolio/StockQuote.java
@@ -1,3 +1,19 @@
+/*
+       Copyright 2017 IBM Corp All Rights Reserved
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 package com.ibm.hybrid.cloud.sample.portfolio;
 
 //Standard HTTP request classes.  Maybe replace these with use of JAX-RS 2.0 client package instead...
@@ -6,7 +22,6 @@ import java.io.InputStream;
 import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -14,10 +29,15 @@ import java.util.Date;
 
 //JSON-P (JSR 353).  The replaces my old usage of IBM's JSON4J (com.ibm.json.java.JSONObject)
 import javax.json.Json;
+import javax.json.JsonArray;
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 
 //JAX-RS 2.0 (JSR 339)
 import javax.ws.rs.core.Application;
+
+import redis.clients.jedis.Jedis;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.PathParam;
@@ -25,27 +45,20 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.Path;
 
-//Jedis (Java library for Redis)
-import redis.clients.jedis.Jedis; //needs jedis-2.9.0.jar on the classpath
-
 
 @ApplicationPath("/")
 @Path("/")
-/** This version of StockQuote talks to API Connect (which talks to Quandl.com) */
+/** This version of StockQuote talks directly to Quandl.com */
 public class StockQuote extends Application {
 	private static final long DAY_IN_MILLISECONDS = 24*60*60*1000;
-
-//	private static final String REDIS_URL = "redis://x:DQJIQGKUYGBCNKTN@sl-us-dal-9-portal.1.dblayer.com:15146"; //Kyle's Redis
-	private static final String REDIS_URL = "redis://x:JTkUgQ5BXo@voting-moth-redis:6379"; //Greg's Redis in CFC
+	private String redis_url = null;
 
 	public static void main(String[] args) {
 		try {
 			if (args.length > 0) {
 				String key = (args.length == 2) ? args[1] : null;
-
 				StockQuote stockQuote = new StockQuote();
 				JsonObject quote = stockQuote.getStockQuote(args[0], key);
-
 //				double value = ((JsonNumber) quote.get("price")).doubleValue();
 				System.out.println(quote.get("price"));
 			} else {
@@ -56,33 +69,46 @@ public class StockQuote extends Application {
 		}
 	}
 
+	public StockQuote() {
+		super();
+
+		try {
+			//The following variable should be set in a Kubernetes secret, and
+			//made available to the app via a stanza in the deployment yaml
+
+			//Example secret creation command: kubectl create secret generic redis
+			//--from-literal=url=redis://x:JTkUgQ5BXo@voting-moth-redis:6379
+			redis_url = System.getenv("url");
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
     @GET
     @Path("/{symbol}")
 	@Produces("application/json")
-	/*  Get the stock quote from the API defined in API Connect.  Behind the scenes,
-	 *  that API's implementation delegates to Quandl.com.
+	/*  Getting stock quote directly from Quandl.
 	 * 
-	 *  Example of data returned for https://api.us.apiconnect.ibmcloud.com/jalcornusibmcom-dev/sb/stocks/
+	 *  Example of data returned for https://www.quandl.com/api/v3/datasets/WIKI/IBM.json?rows=1
 	 *
-	 * { "symbol": "IBM", "date": "2017-01-03", "price": 167.19 }
+     * {"dataset":{"id":9775747,"dataset_code":"IBM","database_code":"WIKI","name":"International Business Machines Corp (IBM) Prices, Dividends, Splits and Trading Volume","description":"End of day open, high, low, close and volume, dividends and splits, and split/dividend adjusted open, high, low close and volume for International Business Machines Corporation (IBM). Ex-Dividend is non-zero on ex-dividend dates. Split Ratio is 1 on non-split dates. Adjusted prices are calculated per CRSP (www.crsp.com/products/documentation/crsp-calculations)\n\nThis data is in the public domain. You may copy, distribute, disseminate or include the data in other products for commercial and/or noncommercial purposes.\n\nThis data is part of Quandl's Wiki initiative to get financial data permanently into the public domain. Quandl relies on users like you to flag errors and provide data where data is wrong or missing. Get involved: connect@quandl.com\n","refreshed_at":"2016-04-26T21:47:46.316Z","newest_available_date":"2016-04-26","oldest_available_date":"1962-01-02","column_names":["Date","Open","High","Low","Close","Volume","Ex-Dividend","Split Ratio","Adj. Open","Adj. High","Adj. Low","Adj. Close","Adj. Volume"],"frequency":"daily","type":"Time Series","premium":false,"limit":1,"transform":null,"column_index":null,"start_date":"1962-01-02","end_date":"2016-04-26","data":[["2016-04-26",148.65,149.79,147.9,149.08,2974825.0,0.0,1.0,148.65,149.79,147.9,149.08,2974825.0]],"collapse":null,"order":"desc","database_id":4922}}
 	 */
-	public JsonObject getStockQuote(@PathParam("symbol") String symbol, @QueryParam("key") String key) throws IOException, URISyntaxException {
-		String uri = "https://api.us.apiconnect.ibmcloud.com/jalcornusibmcom-dev/sb/stocks/"+symbol;
-		if (key != null) uri += "&quandl_key="+key; //only a few invocations per IP address are allowed per day without a key
-
+	public JsonObject getStockQuote(@PathParam("symbol") String symbol, @QueryParam("key") String key) throws IOException {
+		String uri = "https://www.quandl.com/api/v3/datasets/WIKI/"+symbol+".json?rows=1";
+		if (key != null) uri += "&api_key="+key; //only a few invocations per IP address are allowed per day without a key
 		JsonObject quote = null;
 
 		try {
-			System.out.println("Connecting to Redis using URL: "+REDIS_URL);				
+			System.out.println("Connecting to Redis using URL: "+redis_url);				
 
-			URI jedisURI = new URI(REDIS_URL);
+			URI jedisURI = new URI(redis_url);
 			Jedis jedis = new Jedis(jedisURI); //Connect to Redis
 
 			System.out.println("Getting "+symbol+" from Redis");				
 			String cachedValue = jedis.get(symbol); //Try to get it from Redis
 			if (cachedValue == null) { //It wasn't in Redis
 				System.out.println(symbol+" wasn't in Redis");
-				quote = invokeREST("GET", uri); //so go get it like we did before we'd ever heard of Redis
+				quote = extractFromQuandl(invokeREST("GET", uri), symbol); //so go get it like we did before we'd ever heard of Redis
 				jedis.set(symbol, quote.toString()); //Put it Redis so it's there next time we ask
 			} else {
 				System.out.println("Got this from Redis for "+symbol+": "+cachedValue);
@@ -92,7 +118,7 @@ public class StockQuote extends Application {
 
 				if (isStale(quote)) {
 					System.out.println(symbol+" in Redis was too stale");
-					quote = invokeREST("GET", uri); //so go get a less stale value
+					quote = extractFromQuandl(invokeREST("GET", uri), symbol); //so go get a less stale value
 					jedis.set(symbol, quote.toString()); //Put it Redis so it's there next time we ask
 				} else {
 					System.out.println("Used "+symbol+" from Redis");				
@@ -104,7 +130,7 @@ public class StockQuote extends Application {
 			t.printStackTrace();
 
 			//something went wrong using Redis.  Fall back to the old-fashioned direct approach
-			quote = invokeREST("GET", uri);
+			quote = extractFromQuandl(invokeREST("GET", uri), symbol);
 		}
 
 		return quote;
@@ -121,9 +147,22 @@ public class StockQuote extends Application {
 		long difference = today.getTime() - then.getTime();
 
 		return (difference > DAY_IN_MILLISECONDS); //cached quote over a day old (Quandl only returns previous day's closing value)
-	}
+    }
 
-	private JsonObject invokeREST(String verb, String uri) throws IOException {
+	private JsonObject extractFromQuandl(JsonObject obj, String symbol) {
+		JsonObject dataset = (JsonObject) obj.get("dataset");
+		JsonArray outerArray = (JsonArray) dataset.get("data");
+		JsonArray array = (JsonArray) outerArray.get(0);
+
+		JsonObjectBuilder builder = Json.createObjectBuilder();
+		builder.add("symbol", symbol);
+		builder.add("date", array.get(0)); //date is the first element
+		builder.add("price", array.get(4)); //day closing value is the fifth element
+
+		return builder.build();
+	}
+	
+	private static JsonObject invokeREST(String verb, String uri) throws IOException {
 		URL url = new URL(uri);
 
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
