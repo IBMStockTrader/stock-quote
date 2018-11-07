cd9f00b4e343611d7ac888738e4ca4a6d827488d
John Alcorn <jalcorn@us.ibm.com>
2018-11-07 00:44:39 -0500

    mpRestClient interfaces for obtaining stock quotes

---
+++ src/main/java/com/ibm/hybrid/cloud/sample/portfolio/IEXClient.java
@@ -0,0 +1,38 @@
+/*
+       Copyright 2017 IBM Corp All Rights Reserved
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package com.ibm.hybrid.cloud.sample.portfolio;
+
+
+import javax.enterprise.context.Dependent;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@ApplicationPath("/")
+@Path("/")
+@Dependent
+@RegisterRestClient
+/** mpRestClient "remote" interface for the IEX stock quote service */
+public interface IEXClient {
+	@GET
+	@Path("/{symbol}/quote")
+	@Produces("application/json")
+	public Quote getStockQuoteViaIEX(@PathParam("symbol") String symbol);
+}
